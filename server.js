2fdc4f6bf071597fb51206137a7d3495f2e20bc7
Yash Rohatgi <rohatgiy@gmail.com>
2020-12-29 19:45:58 -0500

    deploy

--- server.js
+++ server.js
@@ -97,14 +97,14 @@ app.use('/api/logout', logoutRouter);
 if (process.env.NODE_ENV === "production")
 {
     app.use(express.static('client/build'))
+    app.get('*', (req,res)=>{
+        res.sendFile(path.resolve(__dirname+'client/build/index.html'))
+      })
 }
 
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
-    app.get('*', (req,res)=>{
-        res.sendFile(path.join(__dirname+'client/build/index.html'))
-      })
 });
 
 module.exports = router;
